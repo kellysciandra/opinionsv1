ba3c246dc46cc01fe4103fcff29c16132f2e376d
Kelly Sciandra <kellysciandradev@icloud.com>
2020-04-08 14:05:07 -0400

    deleted modal

--- client/src/chat/ModalExample.js
+++ client/src/chat/ModalExample.js
@@ -1,80 +1,95 @@
+// import React, { Component } from 'react';
+
+// import ChatList from './ChatList';
+// import ChatBox from './ChatBox';
+// import ChatNav from './ChatNav'
+
+
+
+// class MainChat extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       text: '',
+//       username: '',
+//       chats: []
+//     };
+//   }
+
+//   componentDidMount() {
+ 
+//     const username = window.prompt('Username: ', 'Barry White');
+//     this.setState({ username });
+   
+//     var pusher = new Pusher('eaa5420a1b2b42547009', {
+//       cluster: 'us2',
+//       forceTLS: true
+//     });
+//     const channel = pusher.subscribe('chat');
+//     channel.bind('message', data => {
+//       this.setState({ chats: [...this.state.chats, data], test: '' });
+//     });
+//     this.handleTextChange = this.handleTextChange.bind(this);
+//   }
+
+//   handleTextChange(e) {
+//     if (e.keyCode === 13) {
+//       const payload = { 
+//         username: this.state.username,
+//         message: this.state.text
+//       }; console.log(payload)
+//       axios.post('http://localhost:5000/message', payload);
+//       this.setState({ text: ''})
+//     } else {
+//       this.setState({ text: e.target.value });
+//     }
+//   }
+
+//   render() { console.log(this.state)
+//     return (
+//       <div  className='grid'>
+   
+//         <div className='ask_form_card'>
+//           <ChatBox
+//             text={this.state.text}
+//             username={this.state.username}
+//             handleTextChange={this.handleTextChange}
+//           />
+//         </div>
+
+//         <div className='display_chat_text'>
+//           <section className='display_chat_text_header'>
+//           <h1>what do you think of poop?</h1>
+//           </section><br></br><br></br>
+        
+//         <ChatList chats={this.state.chats} />
+//         </div>
+
+//         <div className='chat_nav'>
+//           <ChatNav />
+//         </div>
+      
+//       </div>
+//     );
+//   }
+// }
+
+// export default MainChat;
+
+
 import React, { Component } from 'react';
 
-import ChatList from './ChatList';
-import ChatBox from './ChatBox';
-import ChatNav from './ChatNav'
-
-
-
-class MainChat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '',
-      username: '',
-      chats: []
-    };
-  }
-
-  componentDidMount() {
- 
-    const username = window.prompt('Username: ', 'Barry White');
-    this.setState({ username });
-   
-    var pusher = new Pusher('eaa5420a1b2b42547009', {
-      cluster: 'us2',
-      forceTLS: true
-    });
-    const channel = pusher.subscribe('chat');
-    channel.bind('message', data => {
-      this.setState({ chats: [...this.state.chats, data], test: '' });
-    });
-    this.handleTextChange = this.handleTextChange.bind(this);
-  }
-
-  handleTextChange(e) {
-    if (e.keyCode === 13) {
-      const payload = { 
-        username: this.state.username,
-        message: this.state.text
-      }; console.log(payload)
-      axios.post('http://localhost:5000/message', payload);
-      this.setState({ text: ''})
-    } else {
-      this.setState({ text: e.target.value });
-    }
-  }
-
-  render() { console.log(this.state)
+class ModalExample extends Component {
+  render() {
     return (
-      <div  className='grid'>
-   
-        <div className='ask_form_card'>
-          <ChatBox
-            text={this.state.text}
-            username={this.state.username}
-            handleTextChange={this.handleTextChange}
-          />
-        </div>
-
-        <div className='display_chat_text'>
-          <section className='display_chat_text_header'>
-          <h1>what do you think of poop?</h1>
-          </section><br></br><br></br>
+      <div>
         
-        <ChatList chats={this.state.chats} />
-        </div>
-
-        <div className='chat_nav'>
-          <ChatNav />
-        </div>
-      
       </div>
     );
   }
 }
 
-export default MainChat;
+export default ModalExample;
 
 
 
